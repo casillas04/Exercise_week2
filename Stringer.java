ec1cd7285505af354972cc8356ce9b42cc06677b
casillas04 <lisiyuan04@gmail.com>
2018-08-10 02:05:25 -0700

    Add files via upload

---
+++ Stringer.java
@@ -0,0 +1,9 @@
+package com.company.Siyuan;
+import java.util.*;
+public class Stringer {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        String input=sc.nextLine();
+        for(String st:input.split(","))
+    }
+}
